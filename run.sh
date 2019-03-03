dba2b8a64c63912ace9c29b9aca665b8930413fb
hankliu5 <yliu224@ncsu.edu>
2019-03-03 01:33:40 -0500

    test noatime at the same scheduler run.

--- run.sh
+++ run.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 PATH_TO_READ=$1
-
+MODE=$2
 make
-for thread_num in {1..8}
+
+mount -o relatime -o remount $PATH_TO_READ
+# do relatime (default)
+for thread_num in {1..4}
 do
         touch log.txt
-        echo $thread_num >> log.txt
+        echo ${thread_num} >> log.txt
         for i in {1..128}
         do
                 free &>/dev/null && sync && echo 3 > /proc/sys/vm/drop_caches && free &>/dev/null
-                ./speed_test_multi $thread_num $PATH_TO_READ >> log.txt
+                ./speed_test_multi ${thread_num} ${PATH_TO_READ} >> log.txt
         done
-        echo "finish num_thread $thread_num"
-        python3 extract.py log.txt $2_$thread_num
+        echo "finish num_thread ${thread_num}"
+        python3 extract.py log.txt ${MODE}_${thread_num}
+        rm log.txt
+done
+
+# do noatime
+mount -o noatime -o remount $PATH_TO_READ
+MODE+="_noatime"
+echo "working on noatime"
+
+for thread_num in {1..4}
+do
+        touch log.txt
+        echo ${thread_num} >> log.txt
+        for i in {1..128}
+        do
+                free &>/dev/null && sync && echo 3 > /proc/sys/vm/drop_caches && free &>/dev/null
+                ./speed_test_multi ${thread_num} ${PATH_TO_READ} >> log.txt
+        done
+        echo "finish num_thread ${thread_num}"
+        python3 extract.py log.txt ${MODE}_${thread_num}
         rm log.txt
 done
 mv *.npy out
