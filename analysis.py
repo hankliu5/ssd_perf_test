7ad1fcdccd8dd183c704d8bda1e16dc2c812e8dd
hankliu5 <yliu224@ncsu.edu>
2019-03-03 17:43:15 -0500

    modified graph/data output format

--- analysis.py
+++ analysis.py
@@ -28,32 +28,29 @@ for filename in file_list:
 for platform, platform_results in table.items():
     for scheduler, scheduler_results in platform_results.items():
         # first, compare the same platform/scheduler with different thread_num
-        ls = []
+        m = {}
         print('type: {}_{}'.format(platform, scheduler))
-        print('thread_num\tmin (MB/s)\tavg (MB/s)\tmax (MB/s)\tstd(MB/s)\t')
-        y_top_limit = 0
+        print('thread_num\tmin (MB/s)\tavg (MB/s)\tmedian (MB/s)\tmax (MB/s)\tstd (MB/s)\t')
         for thread_num, thread_num_results in scheduler_results.items():
             max_bandwidth = np.max(thread_num_results)
-            print('{}\t{}\t{}\t{}\t{}'.format(
+            print('{}\t{}\t{}\t{}\t{}\t{}'.format(
                 thread_num,
                 np.min(thread_num_results),
                 np.average(thread_num_results),
+                np.median(thread_num_results),
                 max_bandwidth,
                 np.std(thread_num_results)
             ))
-            y_top_limit = max(y_top_limit, max_bandwidth)
-            ls.append(thread_num_results)
+            m[thread_num] = thread_num_results
 
         fig = plt.figure()
         fig.suptitle('{}_{}'.format(platform, scheduler),
                      fontsize=14, fontweight='bold')
-        y_top_limit = ((int(y_top_limit) // tick) + 1) * tick
         ax = fig.add_subplot(111)
-        ax.boxplot(ls)
+        m = pd.DataFrame.from_dict(m)
+        ax = m.boxplot(ax=ax)
         ax.set_xlabel('thread_num')
         ax.set_ylabel('throughput (MB/s)')
-        ax.set_ylim(bottom=0, top=y_top_limit)
-        ax.set_yticks([t for t in range(0, y_top_limit+1, tick)])
         plt.savefig('plot/{}_{}.png'.format(platform, scheduler))
 
 
@@ -61,45 +58,34 @@ for platform, platform_results in table.items():
 # so we compare different scheduler here:
 for platform, platform_results in table.items():
     m = {}
-    y_top_limit = 0
     for scheduler, scheduler_results in platform_results.items():
-        y_top_limit = max(y_top_limit, np.max(scheduler_results['3']))
         m[scheduler] = scheduler_results['3']
 
     fig = plt.figure()
-    fig.suptitle('{}_{} Comparison with 3 Threads'.format(platform, scheduler),
+    fig.suptitle('{} Comparison with 3 Threads'.format(platform),
                  fontsize=14, fontweight='bold')
-    y_top_limit = ((int(y_top_limit) // tick) + 1) * tick
     ax = fig.add_subplot(111)
     m = pd.DataFrame.from_dict(m)
     ax = m.boxplot(ax=ax)
     ax.set_xlabel('scheduler')
     ax.set_ylabel('throughput (MB/s)')
-    ax.set_ylim(bottom=0, top=y_top_limit)
-    ax.set_yticks([t for t in range(0, y_top_limit+1, tick)])
     plt.savefig('plot/{}_scheduler_compare.png'.format(platform))
 
 
 # Lastly, compare host/device throughput differences
 for scheduler in table['host'].keys():
-    print(scheduler)
     if scheduler in table['device']:
         m = {}
-        y_top_limit = 0
         for platform in ('host', 'device'):
             results = table[platform][scheduler]['3']
-            y_top_limit = max(y_top_limit, np.max(results))
             m[platform] = results
 
         fig = plt.figure()
         fig.suptitle('Platform Comparison in {} with 3 Threads'.format(scheduler),
                         fontsize=14, fontweight='bold')
-        y_top_limit = ((int(y_top_limit) // tick) + 1) * tick
         ax = fig.add_subplot(111)
         m = pd.DataFrame.from_dict(m)
         ax = m.boxplot(ax=ax)
         ax.set_xlabel('scheduler')
         ax.set_ylabel('throughput (MB/s)')
-        ax.set_ylim(bottom=0, top=y_top_limit)
-        ax.set_yticks([t for t in range(0, y_top_limit+1, tick)])
         plt.savefig('plot/platform_{}_compare.png'.format(scheduler))
